acb5b7360f10841637b0d61348dc234f1d5ca0aa
Manuel Santana <msantana@wavefield.cluster>
2025-08-20 14:18:46 -0700

    Circle and Kite Working

--- Geometry/ClosedCurve.cpp
+++ Geometry/ClosedCurve.cpp
@@ -1,46 +1,62 @@
 #include "ClosedCurve.hpp"
-#include "../utils/quadrature.hpp"
+#include <iostream>
+double ClosedCurve::comp_segement_length(double a, double b) {
+    std::vector<double> xps, yps, ab_nodes(num_integration_points);
 
- double ClosedCurve::comp_segement_length(double a, double b, int num_int_points) {
-    std::vector<double> nodes(num_int_points), weights(num_int_points), xps, yps;
-    fejerquadrature1(nodes, weights, num_int_points);
-
-    for (int ii = 0; ii < num_int_points; ii++) {
-        nodes[ii] = ab2cd(-1.0,1.0,a,b,nodes[ii]);
+    for (int ii = 0; ii < num_integration_points; ii++) {
+        ab_nodes[ii] = ab2cd(-1.0, 1.0, a, b, integration_nodes[ii]);
     }
 
-    xpt(nodes, xps, yps);
+    xpt(ab_nodes, xps, yps);
 
     double seg_len = 0.0;
-    for (int ii = 0; ii < num_int_points; ii++) {
-        seg_len += std::sqrt(xps[ii] * xps[ii] + yps[ii] * yps[ii]) * weights[ii];
+    for (int ii = 0; ii < num_integration_points; ii++) {
+        seg_len += std::sqrt(xps[ii] * xps[ii] + yps[ii] * yps[ii]) * integration_weights[ii];
     }
-
-    return seg_len;
+ 
+    return seg_len * ab2cdjac(-1.0, 1.0, a, b);
  }
 
+void ClosedCurve::ref_seg_len(double a, double b, double size_lim, std::vector<double>& seg_endpts) {
+    double len = comp_segement_length(a, b);
+
+    if (len <= size_lim) {
+        seg_endpts.push_back(b);
+        return;
+    }
+
+    double mid = (a + b) / 2.0;
+    ref_seg_len(a, mid, size_lim, seg_endpts);
+    ref_seg_len(mid, b, size_lim, seg_endpts);
+
+}
+
+
 std::vector<double> ClosedCurve::compute_patch_lims(double wavelengths_per_patch,
-    double wavenumber,int num_int_points = 100 ) {
+    double wavenumber) {
     double wavelength = 2 * M_PI / wavenumber;
     // Compute the length of the curve via integration
-   double curve_len = comp_segement_length(0, 2*M_PI, num_int_points);
-    
-   // Split according to wavelengths_per_patch
-   int num_wave_lens = std::ceil(curve_len / wavelength);
-   int num_intervals = std::ceil(num_wave_lens / wavelengths_per_patch);
+    double curve_len = comp_segement_length(0, 2*M_PI);
 
-   std::vector<double> patch_lims; patch_lims.reserve(num_intervals);
-   
-   // TODO: Finish this by putting in the patch intervals.
-   for (int ii = 0; ii < num_intervals; ii++) {
+    // Split according to wavelengths_per_patch
+    int num_wave_lens = std::ceil(curve_len / wavelength);
+    int num_intervals = std::ceil(num_wave_lens / wavelengths_per_patch);
 
-   }
-    
-    // For each patch -> compute the length
-    // If it is greater than wv_per_patch
-    // divide the patch by two, then repeat
-    // 
-    
+    std::vector<double> patch_lims; patch_lims.reserve(num_intervals);
+    // Because ref_seg_len only returns right endpoint we initialize the first left endpoint
+    patch_lims.push_back(0.0);
+
+    for (int ii = 0; ii < num_intervals; ii++) {
+        double left_endpoint = 2 * M_PI * ii / static_cast<double> (num_intervals);
+        double right_endpoint = 2 * M_PI * (ii + 1) / static_cast<double> (num_intervals);
+
+        // DEBUG: Print the left and right endpoints
+        std::cout << "Left endpoint: " << left_endpoint << ", Right endpoint: " << right_endpoint << std::endl;
+
+        ref_seg_len(left_endpoint, right_endpoint, wavelength * wavelengths_per_patch, patch_lims);
+    } 
+
+    return patch_lims;
 }
 
 
@@ -53,10 +69,28 @@ void Circle::xt(const std::vector<double>& ts, std::vector<double>& xs, std::vec
     }
 }
 
-    void Circle::xpt(const std::vector<double>& ts, std::vector<double>& xps, std::vector<double>& yps) const {
-    xps.clear(); yps.clear(); xps.reserve(ts.size()); yps.reserve(ts.size());
+void Circle::xpt(const std::vector<double>& ts, std::vector<double>& xps, std::vector<double>& yps) const {
+xps.clear(); yps.clear(); xps.reserve(ts.size()); yps.reserve(ts.size());
     for (double tval : ts) {
         xps.push_back(-radius * std::sin(tval) + centerx);
         yps.push_back(radius * std::cos(tval) + centery);
+    }
+}
+
+
+
+void Kite::xt(const std::vector<double>& ts, std::vector<double>& xs, std::vector<double>& ys) const {
+    xs.clear(); ys.clear(); xs.reserve(ts.size()); ys.reserve(ts.size());
+    for (double tval : ts) {
+        xs.push_back(A * std::cos(tval) + B * std::cos(2 * tval) + C);
+        ys.push_back(D * std::sin(tval));
+    }
+}
+
+void Kite::xpt(const std::vector<double>& ts, std::vector<double>& xps, std::vector<double>& yps) const {
+    xps.clear(); yps.clear(); xps.reserve(ts.size()); yps.reserve(ts.size());
+    for (double tval : ts) {
+        xps.push_back(-A * std::sin(tval) - 2 * B * std::sin(2 * tval));
+        yps.push_back(D * std::cos(tval));
     }
 }
--- Geometry/ClosedCurve.hpp
+++ Geometry/ClosedCurve.hpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <cmath>
 
+#include "../utils/quadrature.hpp"
+
 class ClosedCurve {
     public:
         // Returns x and y coordinates of points in space.
@@ -9,34 +11,64 @@ class ClosedCurve {
         // Returns tangential derivatives of points in space.
         virtual void xpt(const std::vector<double>& ts, std::vector<double>& xps, std::vector<double>& yps) const = 0;
 
+        // Computes the length of the integration segment corresponding to the parameterization
+        // between a and b
+        double comp_segement_length(double a, double b);
+
+        // Returns a vector of right endpoints dividing ab in half (in parameter space) until
+        //  the segment length is less than size_lim
+        void ref_seg_len(double a, double b, double size_lim, std::vector<double>& seg_endpts);
+
         /// @brief For the 2pi parameterized curve compute the t lims to split it into patches
         /// @param wavelengths_per_patch Number of wavelengths before each patch. usually 1
         /// @param wavenumber The wavenumber for the problem
         /// @param num_int_points Number of points to use in the integration. May need to be larger for larger curves
         /// @return A vector where v[i],v[i+1] gives the t limits for patch i, i = 0,...,v.size() - 1;
-        std::vector<double> compute_patch_lims(double wavelengths_per_patch, double wavenumber, int num_int_points = 100);
-
-        // Computes the length of the integration segment corresponding to the parameterization
-        // between a and b
-        double comp_segement_length(double a, double b, int num_int_points);
-
+        std::vector<double> compute_patch_lims(double wavelengths_per_patch, double wavenumber);
 
         virtual ~ClosedCurve() = default;
+
+        int num_integration_points;
+        std::vector<double> integration_nodes;
+        std::vector<double> integration_weights;
+   
+
+        ClosedCurve(int num_integration_points = 200) : num_integration_points(num_integration_points) {
+            // initialize the integration nodes and weights
+            integration_nodes.resize(num_integration_points);
+            integration_weights.resize(num_integration_points);
+            fejerquadrature1(integration_nodes, integration_weights, num_integration_points);
+        }
+  
 };
 
-class Circle : ClosedCurve { 
+class Circle : public ClosedCurve { 
     public:
         double centerx;
         double centery;
         double radius;
 
-        
-        Circle(double centerx=0.0, double centery=0.0, double radius=1.0) : centerx(centerx),
-            centery(centery), radius(radius) {}
+        Circle(double centerx=0.0, double centery=0.0, double radius=1.0) :  ClosedCurve(),
+         centerx(centerx), centery(centery), radius(radius) { }
 
         void xt(const std::vector<double>& ts, std::vector<double>& xs, std::vector<double>& ys) const override;
         void xpt(const std::vector<double>& ts, std::vector<double>& xps, std::vector<double>& yps) const override;
+};
 
+
+class Kite : public ClosedCurve { 
+    public:
+        // Kite is defined as (x(t), y(t)) with
+        // x(t) = Acos(t) + Bcos(2t) + C
+        // y(t) = Dsin(t)
+        double A, B, C, D;
+
+        // Default parameters correspond to the Colton and Kress kite
+        Kite(double A=1.0, double B=0.65, double C=-0.65, double D=1.5) : ClosedCurve(),
+         A(A), B(B), C(C), D(D) {};
+
+        void xt(const std::vector<double>& ts, std::vector<double>& xs, std::vector<double>& ys) const override;
+        void xpt(const std::vector<double>& ts, std::vector<double>& xps, std::vector<double>& yps) const override;
 };
 
 
